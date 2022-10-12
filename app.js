0b3fcc63b26fd3e0b4ffa347e320ab74b29f58ff
z888e <zoemhrt@gmail.com>
2022-10-12 17:04:37 +0200

    working sounds

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-'use strict';
+var keys = [81, 83, 68, 70, 71, 72, 74, 75, 76]
+
+this.addEventListener('keydown', e => {
+
+    var keyID = e.keyCode ? e.keyCode : e.which;
+
+    if (keys.includes(keyID)) {
+        var sound = document.getElementById(keyID)
+        sound.play();
+    }
+})
+
