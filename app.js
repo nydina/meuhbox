a09bda2cd4b1c34cecfadcd51e75df79ba0892b6
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2024-06-14 12:06:42 +0200

    Allow click on the keys

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-var keys = [81, 83, 68, 70, 71, 72, 74, 75, 76]
+var keys = [81, 83, 68, 70, 71, 72, 74, 75, 76];
 
-this.addEventListener('keydown', e => {
-
-    var keyID = e.keyCode ? e.keyCode : e.which;
-
-    if (keys.includes(keyID)) {
-        var sound = document.getElementById(keyID)
+// Function to handle both keydown and touchstart events
+function playSound(keyID) {
+    var sound = document.getElementById(keyID.toString());
+    if (sound) {
+        sound.currentTime = 0; // Rewind to the start (important for rapid re-triggers)
         sound.play();
     }
-})
+}
 
+// Keydown event listener
+document.addEventListener('keydown', function(e) {
+    var keyID = e.keyCode || e.which;
+    if (keys.includes(keyID)) {
+        playSound(keyID);
+    }
+});
+
+// Touchstart event listener
+keys.forEach(function(keyID) {
+    var keyElement = document.querySelector('[data-key="' + keyID + '"]');
+    if (keyElement) {
+        keyElement.addEventListener('touchstart', function(e) {
+            e.preventDefault(); // Prevent the default touch event behavior
+            playSound(keyID);
+        });
+
+        keyElement.addEventListener('click', function(e) {
+            playSound(keyID);
+        });
+    }
+});
